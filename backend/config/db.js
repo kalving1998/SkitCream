ae4d67251ce97b6b894dfa1e9923a93a4195a44a
Kelvin <uzdutruz@gmail.com>
2026-08-15 23:28:22 -0500

    Agregar puerto de conexion a la base de datos

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const conexion = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Portilla98",
-  database: "skitcream_db",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 conexion.connect(function (error) {
